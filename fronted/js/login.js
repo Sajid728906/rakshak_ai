e26a96c550e5d94f2a0194243bcea200bd49cc62
sajid <sajid@sajids-MacBook-Pro.local>
2026-09-06 12:28:54 +0530

    fix: update login url to vercel

--- fronted/js/login.js
+++ fronted/js/login.js
@@ -46,7 +46,7 @@ document
         try {
 
             const res = await fetch(
-                "http://localhost:5000/api/auth/login",
+                "https://rakshak-ai-alpha.vercel.app/api/auth/login",
                 {
                     method: "POST",
 
